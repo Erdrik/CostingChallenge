9d393560c477999f1cb5ebe8e755446f041db015
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2019-03-14 20:25:23 +0000

    Adding skeletal projects.

---
+++ CostingChallenge/CostingChallengeTests/CostingChallengeTests.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="CostingChallengeTests.cs" company="Samuel Ballard-Adams">
+// Copyright (c) Samuel Ballard-Adams. All rights reserved.
+// Licensed under the GNU General Public License v3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Tests
+{
+    using NUnit.Framework;
+
+    public class CostingChallengeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
